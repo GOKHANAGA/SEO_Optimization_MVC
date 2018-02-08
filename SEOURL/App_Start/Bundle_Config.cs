a52159700f203ac687f18acfbb093ddd079a3c61
Gokhan Irmak <gokhanirmak2@hotmail.com>
2018-02-08 14:15:13 +0300

    Add files via upload

---
+++ SEOURL/App_Start/Bundle_Config.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace SEOURL.App_Start
+{
+    public class Bundle_Config
+    {
+        /*Nuget Web-Optimization eklentisinin eklenmesi gerekli*/
+        /*Çalışması için Global.asax'ta da tanımlaması yapılmalıdır*/
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/css/all").Include("~/Content/bootstrap.min.css", "~/Content/Site.css"));
+
+            /*CDN kullanımını aktif hale getirmek için*/
+            /*CDN kullanırsak diğerleri kullanılamaz hale gelir*/
+
+            bundles.UseCdn = true;
+            string jQueryCDN="https://dsaajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js";
+
+            bundles.Add(new ScriptBundle("~/js/layout",jQueryCDN).Include("~/Scripts/bootstrap.min.js", "~/Scripts/modernizr-{version}.js", "~/Scripts/jquery-[version}.min.js"));
+
+            /*Optimizasyonu açmak için kullanılır yukarıdaki bundle yapılarını sıkıştırıp sayfa yüklenme hızını arttırır*/
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
---
+++ SEOURL/Controllers/NewsController.cs
@@ -0,0 +1,36 @@
+﻿using SEOURL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SEOURL.Controllers
+{
+    public class NewsController : Controller
+    {
+        // GET: News
+        public ActionResult Index()
+        {
+            List<New> myList = DummyNews();
+            return View(myList);
+        }
+
+        public ActionResult Detail(string seoUrl)
+        {
+            New newDetail = DummyNews().Find(n => n.SeoUrl == seoUrl);
+            return View(newDetail);
+        }
+
+        public List<New> DummyNews()
+        {
+            List<New> newsList = new List<New>();
+            newsList.Add(new New { ID = 1, Title = "Van Persie Ayrıldı", Content = "Fenerbahce'de Robin Van Persie Hollanda'ya Transfer oldu", SeoUrl = "van-persie-hollanda-da" });
+            newsList.Add(new New { ID = 2, Title = "Falcon Heavy Fırlatıldı", Content = "Elon Musk yine yaptı yapacağını..Falcon Heavy Uzayda.", SeoUrl = "falcon-heavy-firlatildi" });
+            newsList.Add(new New { ID = 3, Title = "Endonezya sağlık bakanı istifa etti", Content = "Endonezya sağlık bakanı Endo Nezya görevi bıraktığını artık dayanamadığını açıkladı...=D", SeoUrl = "endonezya-saglik-bakani-birakti" });
+            newsList.Add(new New { ID = 4, Title = "Bakın o ünlü ne yaptı", Content = "Ünlü Şarkıcı dün gece yediği yemeği kartla ödedi...", SeoUrl = "bakin-o-unlu-ne-yapti"});
+
+            return newsList;
+        }
+    }
+}
